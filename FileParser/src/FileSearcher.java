278e3aa839f94ccbfad5171ae0ae7e1e55248c40
Sami Rollins <srollins@cs.usfca.edu>
2015-01-05 16:27:41 -0800

    added body of search method

--- FileParser/src/FileSearcher.java
+++ FileParser/src/FileSearcher.java
@@ -10,6 +10,33 @@ public class FileSearcher {
 	
 	public static void containsString(String fileName, String target) {
 		
+		Path path = FileSystems.getDefault().getPath(fileName);
+		
+		boolean found = false;
+		
+		//Regex reference: http://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html
+		Pattern pattern = Pattern.compile("\\d+");
+		
+		try ( Scanner fileScanner = new Scanner(path).useDelimiter(pattern)
+		) {
+			while(fileScanner.hasNext()) {
+				String nextToken = fileScanner.next().toLowerCase();
+				System.out.println("Next token: " + nextToken);
+				if(nextToken.equals(target)) {
+					found = true;
+				}
+				
+			}
+			
+		} catch (IOException e) {
+			System.out.println("FileSearcher.containsString::exception reading file");
+			System.out.println(e.getMessage());
+		}
+		if(found) {
+			System.out.println("Found!");
+		} else {
+			System.out.println("Not found :(");
+		}
 		
 	}
 	
