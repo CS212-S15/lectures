22e8aba625085831783fbb312a6425cfad4d4572
Sami Rollins <srollins@cs.usfca.edu>
2015-01-05 16:26:16 -0800

    Initial upload

---
+++ FileParser/src/FileSearcher.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+public class FileSearcher {
+	
+	
+	public static void containsString(String fileName, String target) {
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		FileSearcher.containsString("input/test1.txt", "here");
+	}
+	
+	
+}
