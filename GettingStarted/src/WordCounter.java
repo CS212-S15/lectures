1750622681db1f231d0d9185fe52a2db81fa6707
Sami Rollins <srollins@cs.usfca.edu>
2015-01-23 18:49:10 -0800

    skeleton for getting started project

---
+++ GettingStarted/src/WordCounter.java
@@ -0,0 +1,20 @@
+/**
+ * A class that counts the number of words that appear in a file.
+ * Requirements:
+ * 1. Uses a Scanner object to read words from a file.
+ * 2. Code should be well designed and modular.
+ * @author srollins
+ *
+ */
+public class WordCounter {
+	
+	public static int countWords(String fileName) {
+		
+		return -1;
+	}
+	
+	
+	public static void main(String[] args) {
+	}
+
+}
