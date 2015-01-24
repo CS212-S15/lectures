5af24edb35d536b8e11963304f15b3dc9fa48855
Sami Rollins <srollins@cs.usfca.edu>
2015-01-23 18:38:42 -0800

    solution with test in separate directory

--- FileParser/src/FileSearcher.java
+++ FileParser/src/FileSearcher.java
@@ -8,10 +8,25 @@ import java.util.regex.Pattern;
 public class FileSearcher {
 	
 	
+	/**
+	 * Takes as input a file and target string and returns true if the file contains the string and false otherwise.
+	 * Tokens in the file are delimited by whitespace.
+	 * @param fileName
+	 * @param target
+	 * @return
+	 */
 	public static boolean containsString(String fileName, String target) {
 		return containsString(fileName, target, "\\s+");
 	}		
 	
+	/**
+	 * Takes as input a file, a target string, and a regular expression (specified as a string) that indicates
+	 * the pattern that delimits tokens in the file. The method returns true if the file contains the target
+	 * string and false otherwise.
+	 * @param fileName
+	 * @param target
+	 * @return
+	 */
 	public static boolean containsString(String fileName, String target, String delimiter) {
 		Path path = FileSystems.getDefault().getPath(fileName);
 		
