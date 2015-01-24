8ecec8c6a26e3824d8316c14e52567e73bdba231
Sami Rollins <srollins@cs.usfca.edu>
2015-01-23 18:51:07 -0800

    removed code from FileSearcher

--- FileParser/src/FileSearcher.java
+++ FileParser/src/FileSearcher.java
@@ -16,7 +16,7 @@ public class FileSearcher {
 	 * @return
 	 */
 	public static boolean containsString(String fileName, String target) {
-		return containsString(fileName, target, "\\s+");
+		return false;
 	}		
 	
 	/**
@@ -28,35 +28,7 @@ public class FileSearcher {
 	 * @return
 	 */
 	public static boolean containsString(String fileName, String target, String delimiter) {
-		Path path = FileSystems.getDefault().getPath(fileName);
-		
-		boolean found = false;
-		
-		//Regex reference: http://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html
-		Pattern pattern = Pattern.compile(delimiter);
-		
-		try ( Scanner fileScanner = new Scanner(path).useDelimiter(pattern)
-		) {
-			while(fileScanner.hasNext()) {
-				String nextToken = fileScanner.next().toLowerCase();
-				if(nextToken.equals(target)) {
-					found = true;
-				}
-				
-			}
-			
-		} catch (IOException e) {
-			System.out.println("FileSearcher.containsString::exception reading file");
-			System.out.println(e.getMessage());
-		}
-		/*
-		if(found) {
-			System.out.println("Found!");
-		} else {
-			System.out.println("Not found :(");
-		}
-		*/
-		return found;
+		return false;
 	}
 	
 	
