d3f7a38d633a6979604570bdab76f7f6741609a3
Sami Rollins <srollins@cs.usfca.edu>
2015-01-28 13:53:43 -0800

    fully functional word count with pattern

--- GettingStarted/src/WordCounter.java
+++ GettingStarted/src/WordCounter.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * A class that counts the number of words that appear in a file.
@@ -19,15 +21,38 @@ public class WordCounter {
 	 */
 	public static int countWords(String fileName) {
 		
-		
+		int count = 0;
 		Path path = FileSystems.getDefault().getPath(fileName);		
-		Scanner fileScanner = new Scanner(path);
-		
-		return -1;
-	}
 	
+		
+		Pattern pattern = Pattern.compile("\\d+");
+		//Pattern pattern = Pattern.compile("[0-9]");
+		
+		try (Scanner fileScanner = new Scanner(path).useDelimiter(pattern)){
+		//try (Scanner fileScanner = new Scanner(path)){
+			
+			while(fileScanner.hasNext()) {
+				System.out.println(fileScanner.next());
+				count++;
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return count;
+	}	
 	
 	public static void main(String[] args) {
+		
+		System.out.println(countWords("input/test1.txt"));
+		
+		/*
+		int result = countWords("input/test1.txt");
+		System.out.println("Result: " + result);
+		*/
+		
 		
 	}
 
