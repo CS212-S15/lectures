f502a5f2fa209585997f9621d110f8aefafa14df
Sami Rollins <srollins@cs.usfca.edu>
2015-03-02 14:42:13 -0800

    adding Thread code from class 3/2

--- Threads/src/BasicThreads.java
+++ Threads/src/BasicThreads.java
@@ -3,12 +3,12 @@ public class BasicThreads {
 
 	public static void main(String[] args) {
 
-		//instantiate a Runnable object
-		InfinitePrinter p1 = new InfinitePrinter("T1");
-		InfinitePrinter p2 = new InfinitePrinter("T2");
-				
+		//instantiate a Runnable object				
 		//pass the runnable to a Thread object
+		InfinitePrinter p1 = new InfinitePrinter("T1");
 		Thread t1 = new Thread(p1);
+		
+		InfinitePrinter p2 = new InfinitePrinter("T2");
 		Thread t2 = new Thread(p2);
 		
 		//start the Thread
@@ -16,6 +16,19 @@ public class BasicThreads {
 		t2.start();
 				
 		/* TBD: using join */
+		try {
+		
+			
+			
+			t1.join();
+			t2.join();
+
+		
+		
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		
 		System.out.println("Finished");
 		
--- Threads/src/SummationExample.java
+++ Threads/src/SummationExample.java
@@ -12,24 +12,46 @@ public class SummationExample {
 		
 		
 		/* TBD: list of threads */
-		/* TBD: executor service */
+		//ArrayList<Thread> threadList = new ArrayList<>();
+		
+		/* TBD: executor service */		
+		//ExecutorService threadPool = Executors.newFixedThreadPool(10);
 		
 		
 		long start = System.currentTimeMillis(); //retrieve current time when starting calculations
 		
 		for(long i = 1; i < MAX; i++) {
 			/* TBD: sequential execution */
+			Summation.calcSummation(i);
 			
 			/* TBD: one thread per task */
+			//Thread t = new Thread(new Summation(i));
+			//threadList.add(t);
+			//t.start();
+			
 			
 			/* TBD: thread pool */
+			//threadPool.execute(new Summation(i));			
 		
 		}
 		
 		/* TBD: join on all threads */
+		//for(Thread t: threadList) {
+			//try {
+				//t.join();
+			//} catch (InterruptedException e) {
+				//e.printStackTrace();
+			//}
+		//}
+		
 		
 		/* TBD: shutdown executor service */
-		
+		//threadPool.shutdown();
+		//try {
+			//threadPool.awaitTermination(2, TimeUnit.MINUTES);
+		//} catch (InterruptedException e) {
+			//e.printStackTrace();
+		//}
 		
 		long end = System.currentTimeMillis(); //retrieve current time when finishing calculations
 		System.out.println("time: " + (end-start));
