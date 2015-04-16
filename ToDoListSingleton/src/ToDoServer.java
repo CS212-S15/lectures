0196f5e138fe937354a8f1d46cb09a28c8dea657
Sami Rollins <srollins@cs.usfca.edu>
2015-04-15 19:33:02 -0700

    adding two ToDo list examples, one that uses a singleton and one that uses a servlet context

---
+++ ToDoListSingleton/src/ToDoServer.java
@@ -0,0 +1,35 @@
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+
+public class ToDoServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		Server server = new Server(8080);
+        
+        //create a ServletHander to attach servlets
+        ServletContextHandler servhandler = new ServletContextHandler(ServletContextHandler.SESSIONS);        
+        server.setHandler(servhandler);
+ 
+        servhandler.addServlet(LoginServlet.class, "/login");
+        servhandler.addServlet(VerifyUserServlet.class, "/verifyuser");
+        servhandler.addServlet(ListServlet.class, "/list");
+
+        //set the list of handlers for the server
+        server.setHandler(servhandler);
+        
+        //start the server
+        server.start();
+        server.join();
+
+	}
+
+}
